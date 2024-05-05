84ab2ed8d094e5c92fd200645bba1534c8efe57c
nallujin <sako75hz@gmail.com>
2024-05-05 11:02:15 +0700

    Add initial calculator functions

---
+++ script.js
@@ -0,0 +1,32 @@
+// Variables
+let num1;
+let num2;
+let operator = "";
+
+
+// Calculator Functions
+// For addition
+function add(a, b){
+    return a + b;
+}
+
+// For subtraction
+function subtract(a, b){
+    return a - b;
+}
+
+// For multiplication
+function multiply(a, b){
+    return a * b;
+}
+
+// For division
+function divide(a, b){
+    return a / b;
+}
+
+//////////////////////////////////
+
+function operate(op, a, b){
+
+}
