ee7e316e51aadbaf8304b8f8990ef4eb33d215cc
nallujin <sako75hz@gmail.com>
2024-05-15 00:58:54 +0700

    Add keyboard support

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let operator = "";
 const currentDisplayNumber = document.querySelector(".currentNumber");
 const previousDisplayNumber = document.querySelector(".previousNumber");
 
+window.addEventListener("keydown", handleKeyPress)
+
 const equal = document.querySelector(".equal");
 equal.addEventListener("click", ()=>{
     if (currentNum !== "" && previousNum !== ""){
@@ -118,5 +120,44 @@ function addDecimal(){
     if(!currentNum.includes(".")){
         currentNum += ".";
         currentDisplayNumber.textContent += currentNum;
+    }
+}
+
+function handleKeyPress(e){
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9){
+        handleNumber(e.key);
+    }
+    if (
+        e.key === "Enter" ||
+        (e.key === "=" && currentNum !== "" && previousNum !== "")
+    ) {
+        calculate();
+    }
+    if (e.key === "+" || e.key === "-" || e.key === "/") {
+        handleOperator(e.key);
+    }
+    if (e.key === "*") {
+        handleOperator("x");
+    }
+    if (e.key === ".") {
+        addDecimal();
+    }
+    if (e.key === "Backspace") {
+        handleDelete();
+    }
+}
+
+function handleDelete() {
+    if (currentNum !== "") {
+        currentNum = currentNum.slice(0, -1);
+        currentDisplayNumber.textContent = currentNum;
+        if (currentNum === "") {
+            currentDisplayNumber.textContent = "0";
+        }
+    }
+    if (currentNum === "" && previousNum !== "" && operator === "") {
+        previousNum = previousNum.slice(0, -1);
+        currentDisplayNumber.textContent = previousNum;
     }
 }
